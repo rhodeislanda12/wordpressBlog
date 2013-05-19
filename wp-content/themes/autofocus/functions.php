1c9025f99c3ef7e5a8ffda84ba7fd312b2a7ad96
Michelle <michelle.e.rittner@gmail.com>
2013-05-19 13:39:59 -0400

    removed RSS feed from top nav

--- wp-content/themes/autofocus/functions.php
+++ wp-content/themes/autofocus/functions.php
@@ -461,7 +461,7 @@ function sandbox_globalnav() {
     echo '<div id="menu"><ul><li class="page_item"><a href="'. get_settings('home') .'/" title="'. get_bloginfo('name') .'" rel="home">Home</a></li>';
     $menu = wp_list_pages('title_li=&sort_column=menu_order&echo=0'); // Params for the page list in header.php
     echo str_replace(array("\r", "\n", "\t"), '', $menu);
-    echo '<li class="page_item"><a href="'. get_bloginfo_rss('rss2_url') .'">RSS</a></li></ul></div>';
+    echo '<li class="page_item"></li></ul></div>';
 }
 
 // Post Attachment image function. Image URL for CSS Background. 
